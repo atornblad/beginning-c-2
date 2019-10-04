277604b1bfcfb058fe5acebf058e8a639988089a
Anders Tornblad <anders.tornblad@gmail.com>
2019-10-04 15:29:02 +0200

    Initializing global integers working fine

--- src/compilation/Generator.js
+++ src/compilation/Generator.js
@@ -6,14 +6,47 @@ const INT_SIZE = 4;
 
 const typeSizes = {
     'char' : 1,
+    'char|signed' : 1,
+    'char|unsigned' : 1,
     'int' : INT_SIZE,
-    'int,long' : 4,
+    'int|signed' : INT_SIZE,
+    'int|unsigned' : INT_SIZE,
+    'int|long' : 4,
     'long' : 4,
-    'int,short' : 2,
-    'short' : 2
+    'int|long|unsigned' : 4,
+    'long|unsigned' : 4,
+    'int|long|signed' : 4,
+    'long|signed' : 4,
+    'int|short' : 2,
+    'short' : 2,
+    'int|short|signed' : 2,
+    'short|signed' : 2,
+    'int|short|unsigned' : 2,
+    'short|unsigned' : 2
 };
 
-const getTypeSize = function(defType) {
+const viewSetters = {
+    'char' : 'setInt8',
+    'char|signed' : 'setInt8',
+    'char|unsigned' : 'setUint8',
+    'int' : 'setInt32',
+    'int|signed' : 'setInt32',
+    'int|unsigned' : 'setUint32',
+    'int|long' : 'setInt32',
+    'long' : 'setInt32',
+    'int|long|unsigned' : 'setUint32',
+    'long|unsigned' : 'setUint32',
+    'int|long|signed' : 'setInt32',
+    'long|signed' : 'setInt32',
+    'int|short' : 'setInt16',
+    'short' : 'setInt16',
+    'int|short|signed' : 'setInt16',
+    'short|signed' : 'setInt16',
+    'int|short|unsigned' : 'setUint16',
+    'short|unsigned' : 'setUint16'
+};
+
+const getTypeInfo = function(defType) {
     switch (defType.type) {
         case 'PointerType':
             return POINTER_SIZE;
@@ -22,24 +55,27 @@ const getTypeSize = function(defType) {
             typeAndModifiers.delete('const');
             typeAndModifiers.delete('static');
             typeAndModifiers.delete('volatile');
-            typeAndModifiers.delete('signed');
-            typeAndModifiers.delete('unsigned');
             const typeName = Array.from(typeAndModifiers);
             typeName.sort();
-            return typeSizes[typeName.join(',')];
+            const finalName = typeName.join('|');
+            return {
+                name : finalName,
+                size : typeSizes[finalName],
+                viewSetter : viewSetters[finalName]
+            };
         default:
             throw new Error(`Unknown defType.type '${defType.type}'`);
     }
 };
 
-const generateSetVariableAt = async function(address, size, expression) {
+const generateSetVariableAt = async function(scope, address, info, expression) {
     switch (expression.type) {
         case 'Literal':
             // TODO: Convert to correct binary format
             return `{
-    const temp = new ArrayBuffer(4);
+    const temp = new ArrayBuffer(${info.size});
     const view = new DataView(temp);
-    view.setInt32(0, ${expression.value}, true);
+    view.${info.viewSetter}(0, ${expression.value}, true);
     this.$memory.set(new Uint8Array(temp), ${address});
 }`;
         default:
@@ -47,27 +83,27 @@ const generateSetVariableAt = async function(address, size, expression) {
     }
 }
 
-const generateSetVariable = async function(decl, expression) {
+const generateSetVariable = async function(scope, decl, expression) {
     if (decl.address !== (void 0)) {
-        return await generateSetVariableAt(decl.address, decl.size, expression);
+        return await generateSetVariableAt(scope, decl.address, decl.info, expression);
     }
 };
 
 const addGlobalVariableDeclaration = async function(declaration, scope, result) {
-    const size = getTypeSize(declaration.defType);
+    const info = getTypeInfo(declaration.defType);
     const address = result.$memBottom;
 
     scope[declaration.name] = {
         declaration: declaration,
         type : declaration.defType.name || declaration.defType.type,
         address: address,
-        size : size
+        info : info
     };
 
-    result.$memBottom += size;
+    result.$memBottom += info.size;
 
     if (declaration.value !== (void 0)) {
-        result.$prologCode += await generateSetVariable(scope[declaration.name], declaration.value);
+        result.$prologCode += await generateSetVariable(scope, scope[declaration.name], declaration.value);
     }
 };
 
